75c2a1d12e735bcbeb776fda8286d4f38cb2f538
tranduyquang <tranduyquang2205@gmail.com>
2024-06-30 21:50:06 +0700

    update

---
+++ acb.py
@@ -0,0 +1,170 @@
+import requests
+import json
+import random
+import concurrent.futures
+import unidecode
+import time
+class ACB:
+    def __init__(self, username, password, account_number,proxy_list=None):
+        self.connect = None  # You can initialize your database connection here
+        self.clientId = 'iuSuHYVufIUuNIREV0FB9EoLn9kHsDbm'
+        self.URL = {
+            "LOGIN": "https://apiapp.acb.com.vn/mb/auth/tokens",
+        }
+        self.time_login = time.time()
+        self.token = ""
+        self.password = password
+        self.username = username
+        self.account_number = account_number
+        self.is_login = False
+        self.proxy_list = proxy_list
+        self.proxies = None
+        if(self.proxy_list):
+            self.proxy = random.choice(self.proxy_list)
+            self.proxies = {
+            'http': f'http://{self.proxy}',
+            'https': f'https://{self.proxy}'
+            }
+    def handleLogin(self):
+        data = {
+            'clientId': self.clientId,
+            'username': self.username,
+            'password': self.password
+        }
+        return self.curl_post(self.URL["LOGIN"], data)
+
+    def get_bank_name(self, ben_account_number, bank_name):
+        if not self.is_login or time.time() - self.time_login > 300:
+            self.login()
+        bank_code = self.mapping_bank_code(bank_name)
+        status = False
+        message = 'Error exception'
+        data = {}
+        url = f'https://apiapp.acb.com.vn/mb/legacy/ss/cs/bankservice/transfers/accounts/{ben_account_number}?bankCode={bank_code}&accountNumber={self.account_number}'
+
+        count = 0
+        while True:
+            bankName = self.curl_get(url)
+            if 'Unauthorized' not in bankName:
+                data = bankName
+                status = True
+                message = 'Successfully'
+                break
+            else:
+                login = self.login()
+
+            count += 1
+            if count > 5:
+                message = 'Connect false'
+                break
+
+        return {'status': status, 'message': message, 'data': data}
+    def convert_to_uppercase_no_accents(self,text):
+        # Remove accents
+        no_accents = unidecode.unidecode(text)
+        # Convert to uppercase
+        return no_accents.upper()
+    def check_bank_name(self,ben_account_number, bank_name, ben_account_name):
+        get_name_from_account = self.get_bank_name(ben_account_number, bank_name)
+        if 'data' in get_name_from_account and 'data' in get_name_from_account['data']:
+            if get_name_from_account['data']['data'] and 'ownerName' in get_name_from_account['data']['data']:
+                input_name = self.convert_to_uppercase_no_accents(ben_account_name).lower().strip()
+                output_name = get_name_from_account['data']['data']['ownerName'].lower().strip()
+                if output_name == input_name or output_name.replace(' ','') == input_name:
+                    return True
+                else:
+                    return output_name
+        return False
+    
+    def load_user(self, username):
+        # Implement database queries to load user here
+        pass
+
+    def login(self):
+        if not self.username or not self.username:
+            return {'success': 0, 'msg': 'Vui lòng nhập đầy đủ thông tin'}
+        user = self.load_user(self.username)
+
+        res = self.handleLogin()
+        if 'accessToken' in res:
+            self.token = res['accessToken']
+            data = json.dumps(res)
+            if not user:
+                # Implement database insert here
+                pass
+            else:
+                # Implement database update here
+                pass
+            self.is_login = True
+            self.time_login = time.time()
+            return {'success': 1, 'msg': 'Đăng nhập thành công'}
+        else:
+            return {'success': 0, 'msg': res['message']} 
+
+    def curl_get(self, url):
+        try:
+            headers = self.header_null()
+            response = requests.get(url, headers=headers, timeout=60,proxies=self.proxies)
+            result = response.json()
+            return result
+        except Exception as e:
+            return False
+
+    def curl_post(self, url, data=None):
+        try:
+            headers = self.header_null()
+            response = requests.post(url, headers=headers, json=data, timeout=60,proxies=self.proxies)
+            result = response.json()
+            return result
+        except Exception as e:
+            return e
+    def header_null(self):
+            header = {
+                'Accept': 'application/json',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Accept-Language': 'vi',
+                'Connection': 'keep-alive',
+                'Content-Type': 'application/json',
+                'sec-ch-ua-mobile': '?0',
+                'Sec-Fetch-Dest': 'empty',
+                'Sec-Fetch-Mode': 'cors',
+                'Sec-Fetch-Site': 'same-site',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
+            }
+            if self.token:
+                header['Authorization'] = 'Bearer ' + self.token
+
+            return header
+    def mapping_bank_code(self,bank_name):
+        with open('banks.json','r', encoding='utf-8') as f:
+            data = json.load(f)
+        for bank in data['data']:
+            if bank['shortName'].lower() == bank_name.lower():
+                return bank['bin']
+        return None
+# def process_line(line):
+#     parts = line.split()
+#     account_name = ' '.join(parts[:-2])
+#     account_number = parts[-2]
+#     bank_name = parts[-1]
+#     check_bank_name =  acb.check_bank_name(account_number, bank_name, account_name), line
+#     return check_bank_name
+
+# username = "0792818254"
+# password = "Oanh888999"
+# account_number="34097977"
+# proxy_list = []
+# acb = ACB(username, password, account_number,proxy_list)
+
+# login = acb.login()
+
+# print(login)
+# with open('test_cases.txt', 'r',encoding="utf8") as file:
+#     lines = file.readlines()
+
+# with concurrent.futures.ThreadPoolExecutor() as executor:
+#     futures = [executor.submit(process_line, line) for line in lines]
+#     for future in concurrent.futures.as_completed(futures):
+#         result, line = future.result()
+#         print(f'{line.strip()}, || {result}')
+
---
+++ app.py
@@ -0,0 +1,164 @@
+from fastapi import FastAPI, HTTPException
+from concurrent.futures import ThreadPoolExecutor, as_completed, TimeoutError
+import uvicorn
+from pydantic import BaseModel
+import random
+import time
+import configparser
+from acb import ACB
+from mbbank_biz import MBBANK
+from seabank import SeaBank
+from techcombank_biz import Techcombank
+from vietabank import VietaBank
+from vietinbank import VTB
+from api_response import APIResponse
+import sys
+import traceback
+
+# Read configuration from config file
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+def parse_proxy_list(proxy_list_str):
+    if proxy_list_str.lower() in ['none', 'empty']:
+        return None
+    return proxy_list_str.split(',')
+
+acb = ACB(
+    config['ACB']['username'], 
+    config['ACB']['password'], 
+    config['ACB']['account_number'], 
+    parse_proxy_list(config['ACB']['proxy_list'])
+)
+mbbank = MBBANK(
+    config['MBBANK']['corp_id'], 
+    config['MBBANK']['username'], 
+    config['MBBANK']['password'], 
+    config['MBBANK']['account_number'], 
+    parse_proxy_list(config['MBBANK']['proxy_list'])
+)
+tcb = Techcombank(
+    config['Techcombank']['username'], 
+    config['Techcombank']['password'], 
+    config['Techcombank']['account_number'], 
+    parse_proxy_list(config['Techcombank']['proxy_list'])
+)
+vtb = VTB(
+    config['VTB']['username'], 
+    config['VTB']['password'], 
+    config['VTB']['account_number'], 
+    parse_proxy_list(config['VTB']['proxy_list'])
+)
+seabank = SeaBank(
+    config['SeaBank']['username'], 
+    config['SeaBank']['password'], 
+    config['SeaBank']['account_number'], 
+    parse_proxy_list(config['SeaBank']['proxy_list'])
+)
+vietabank = VietaBank(
+    config['VietaBank']['username'], 
+    config['VietaBank']['password'], 
+    config['VietaBank']['account_number'], 
+    parse_proxy_list(config['VietaBank']['proxy_list'])
+)
+
+banks = [acb, mbbank]
+
+def check_bank(bank, account_number, bank_name, account_name):
+    try:
+        return bank.check_bank_name(account_number, bank_name, account_name)
+    except Exception as e:
+        response = str(e)
+        print(traceback.format_exc())
+        print(sys.exc_info()[2])
+        return APIResponse.json_format(response)
+
+app = FastAPI()
+
+class BankInfo(BaseModel):
+    account_number: str
+    bank_name: str
+    account_name: str
+
+@app.post('/check_bank_name', tags=["check_bank_name"])
+def check_bank_name(input: BankInfo):
+    account_number = input.account_number
+    bank_name = input.bank_name
+    account_name = input.account_name
+
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        selected_banks = random.sample(banks, 2)
+        futures = [executor.submit(check_bank, bank, account_number, bank_name, account_name) for bank in selected_banks]
+        start_time = time.time()
+
+        try:
+            for future in as_completed(futures, timeout=5):
+                try:
+                    result = future.result()
+                    if result == True:
+                        return APIResponse.json_format({'result': result, 'bank': str(selected_banks[futures.index(future)].__class__.__name__)})
+                    elif result == False:
+                        return APIResponse.json_format({'result': result, 'bank': str(selected_banks[futures.index(future)].__class__.__name__)})
+                    else:
+                        return APIResponse.json_format({'result': False, 'true_name': result, 'bank': str(selected_banks[futures.index(future)].__class__.__name__)})
+                except Exception as e:
+                    response = str(e)
+                    print(traceback.format_exc())
+                    print(sys.exc_info()[2])
+                    return APIResponse.json_format(response)
+        except TimeoutError:
+            return APIResponse.json_format({'message': 'timeout'})
+
+        if time.time() - start_time >= 5:
+            # Retry with another set of banks
+            remaining_banks = [bank for bank in banks if bank not in selected_banks]
+            futures = [executor.submit(check_bank, bank, account_number, bank_name, account_name) for bank in remaining_banks]
+
+            try:
+                for future in as_completed(futures, timeout=5):
+                    try:
+                        result = future.result()
+                        if result == True:
+                            return APIResponse.json_format({'result': result, 'bank': str(selected_banks[futures.index(future)].__class__.__name__)})
+                        elif result == False:
+                            return APIResponse.json_format({'result': result, 'bank': str(selected_banks[futures.index(future)].__class__.__name__)})
+                        else:
+                            return APIResponse.json_format({'result': False, 'true_name': result, 'bank': str(selected_banks[futures.index(future)].__class__.__name__)})
+                    except Exception as e:
+                        response = str(e)
+                        print(traceback.format_exc())
+                        print(sys.exc_info()[2])
+                        return APIResponse.json_format(response)
+            except TimeoutError:
+                return APIResponse.json_format({'result': False ,'message': 'timeout'})
+
+        return APIResponse.json_format({'result': False})
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=3000)
+
+
+    # account_number = "024042205"
+    # bank_name = "MBBank"
+    # account_name = "tran duy quang"
+
+    # with ThreadPoolExecutor(max_workers=2) as executor:
+    #     while True:
+    #         selected_banks = random.sample(banks, 2)
+    #         futures = [executor.submit(check_bank, bank, account_number, bank_name, account_name) for bank in selected_banks]
+    #         start_time = time.time()
+
+    #         for future in as_completed(futures, timeout=5):
+    #             try:
+    #                 result = future.result()
+    #                 if result:
+    #                     print({'result': True, 'bank': str(selected_banks[futures.index(future)].__class__.__name__)})
+    #                     break
+    #             except Exception as e:
+    #                 continue
+
+    #         if time.time() - start_time >= 5:
+    #             continue
+
+    #         print(({'result': False}))
+    #         break
