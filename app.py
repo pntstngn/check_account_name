75a85a450e5bd623f905fff1b35fbe67e9a53172
tranduyquang <tranduyquang2205@gmail.com>
2024-07-05 14:50:34 +0700

    update

--- app.py
+++ app.py
@@ -65,6 +65,7 @@ vietabank = VietaBank(
 banks = [acb,mbbank,tcb,vietabank,seabank,vtb]
 
 def check_bank(bank, account_number, bank_name, account_name):
+    print(bank.__class__.__name__)
     return bank.check_bank_name(account_number, bank_name, account_name)
 
 
